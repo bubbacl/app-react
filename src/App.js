37d13dfd98f5ea914a08d4aef7d5b36d04bd46c2
Rodrigo Burgos <rodrigoburgoscl@gmail.com>
2020-04-12 00:43:04 -0400

    Creando el primer componente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// function Hello (props) {
+//     return <h1>{props.title}</h1>
+// }
+
+// const Hello = (props) => <h1>{props.title}</h1>
+
+class Hello extends Component {
+    render () {
+        return <h1>{this.props.title}</h1>
+    }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>¡Hola mundo!</h1>
+        <Hello title='Hola desde un props' />
         <p>Vamos a aprender ReactJS</p>
         <a
           className="App-link"
